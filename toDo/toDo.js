373b2e4dae2ed56c6a2deda3b43eb38d1bc6210b
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-09-08 18:09:32 +0300

    Add delete method for ToDo

--- toDo/toDo.js
+++ toDo/toDo.js
@@ -1,31 +1,152 @@
-import { LightningElement, api, wire } from 'lwc';
-import { createRecord } from 'lightning/uiRecordApi';
-import ToDo from '@salesforce/apex/ToDoController.uncompletedToDo';
-import RECORD_ID from '@salesforce/schema/Todo__c.RecordTypeId';
+import { LightningElement, api, track, wire} from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex';
+import { deleteRecord } from 'lightning/uiRecordApi';
+import { reduceErrors } from 'c/ldsUtils';
+import uncompletedToDo from '@salesforce/apex/ToDoControllerSecond.uncompletedToDo';
+import completedToDo from '@salesforce/apex/ToDoControllerSecond.completedToDo';
+import allToDo from '@salesforce/apex/ToDoControllerSecond.allToDo';
 
+import getSingleToDo from '@salesforce/apex/ToDoControllerSecond.getSingleToDo';
+import getSingleSubToDo from '@salesforce/apex/ToDoControllerSecond.getSingleSubToDo';
 
 export default class toDo extends LightningElement {
-//fields for add
-//
-//     toDoNAme;
-//     descToDo;
-//     priority;
-     uncompletedToDoList;
-//    RECORD_ID = Schema.SObjectType.Todo__c.getRecordTypeInfosByName()
+
+    error;
+    //todos for updating
+    singleToDo;
+    singleSubToDo;
+
+    uncompletedToDoList;
+    completedToDoList;
+    //спросить ка рефрешнуть весь апекс
+    refreshToDoList =[];
+    allToDoList;
+
+ /*   connectedCallback(){
+    uncompletedToDo().then(resp=>{
+        this.uncompletedToDoList = resp.data;
+    }).catch(err => console.log(err))
+    console.log('Connected');
+}*/
+//call apex methods
+    connectedCallback(){
+        uncompletedToDo()
+            .then(result => {
+                this.uncompletedToDoList = result;
+                this.error = undefined;
+            })
+            .catch(error => {
+                this.error = error;
+                this.uncompletedToDoList = undefined;
+            });
+
+        completedToDo()
+            .then(result => {
+                this.completedToDoList = result;
+                this.error = undefined;
+            })
+            .catch(error => {
+                this.error = error;
+                this.completedToDoList = undefined;
+            });
+
+//наверное не нужен для каждого листа свой надо будет делать
+        allToDo()
+            .then(result => {
+                this.allToDoList = result;
+                this.error = undefined;
+            })
+            .catch(error => {
+                this.error = error;
+                this.allToDoList = undefined;
+            });
 
 
-    //UNCOMPLETED TODO LIST
-    connectedCallback() {
-        this.loadToDo();
+        console.log('Connected');
     }
 
 
 
-    loadToDo() {
-        ToDo().then(result => {
-            this.uncompletedToDoList = result;
+    //delete uncompleted record
+
+    deleteToDo(event) {
+        const recordId = event.target.dataset.recordid;
+        deleteRecord(recordId)
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Account deleted',
+                        variant: 'success'
+                    })
+                );
+                return refreshApex(this.uncompletedToDoList);
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error deleting record',
+                        message: reduceErrors(error).join(', '),
+                        variant: 'error'
+                    })
+                );
             });
     }
+
+    deleteCompletedToDo(event) {
+        const recordId = event.target.dataset.recordid;
+        event.target.accessKeyLabel
+        deleteRecord(recordId)
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Account deleted',
+                        variant: 'success'
+                    })
+                );
+                return refreshApex(this.completedToDoList);
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error deleting record',
+                        message: reduceErrors(error).join(', '),
+                        variant: 'error'
+                    })
+                );
+            });
+    }
+
+
+
+
+    /*   @wire(allToDo)
+  wiredAllToDo(result) {
+      this.allToDoList = result;
+      if (result.data) {
+          this.todoData = result.data;
+          this.error = undefined;
+      } else if (result.error) {
+          this.error = result.error;
+          this.todoData = undefined;
+      }
+  }
+
+ @wire(uncompletedToDo)
+  wiredUncompletedToDo(result) {
+  console.log(result, 'result');
+      this.uncompletedToDoList = result;
+      console.log(result.data);
+      if (result.data) {
+          this.todoData = result.data;
+          this.error = undefined;
+      } else if (result.error) {
+          this.error = result.error;
+          this.todoData = undefined;
+      }
+  }*/
 
 
 }
