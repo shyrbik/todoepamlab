9ab5a472c54a1ff92b679bef2a948428028c1492
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-09-11 11:04:44 +0300

    Add create method, subToDo generation

--- toDo/toDo.js
+++ toDo/toDo.js
@@ -1,76 +1,192 @@
 import { LightningElement, api, track, wire} from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { refreshApex } from '@salesforce/apex';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { deleteRecord } from 'lightning/uiRecordApi';
+import { createRecord } from 'lightning/uiRecordApi';
 import { reduceErrors } from 'c/ldsUtils';
 import getUncompletedToDo from '@salesforce/apex/ToDoControllerSecond.getUncompletedToDo';
 import getCompletedToDo from '@salesforce/apex/ToDoControllerSecond.getCompletedToDo';
+import refreshLists from '@salesforce/apex/ToDoControllerSecond.refreshLists';
 import getAllToDo from '@salesforce/apex/ToDoControllerSecond.getAllToDo';
-//import completedToDo from '@salesforce/apex/ToDoControllerSecond.getCompletedToDo';
-//import {getSObjectValue} from "@salesforce/apex";
-//import uncompletedSubToDo from '@salesforce/apex/ToDoControllerSecond.uncompletedSubToDo';
-//import completedSubToDo from '@salesforce/apex/ToDoControllerSecond.completedSubToDo';
-
-
-import getSingleToDo from '@salesforce/apex/ToDoControllerSecond.getSingleToDo';
-import getSingleSubToDo from '@salesforce/apex/ToDoControllerSecond.getSingleSubToDo';
-
-import ToDo from '@salesforce/apex/ToDoController.uncompletedToDo';
+import getAllSubToDo from '@salesforce/apex/ToDoControllerSecond.getAllSubToDo';
 import TODO_OBJECT from '@salesforce/schema/Todo__c';
 import TODO_NAME from '@salesforce/schema/Todo__c.Name';
 import TODO_DESCRIPTION from '@salesforce/schema/Todo__c.Description__c';
 import TODO_CLOSEDATE from '@salesforce/schema/Todo__c.Close_Date__c';
 import TODO_PRIORITY from '@salesforce/schema/Todo__c.Priority__c';
 import TODO_STATUS from '@salesforce/schema/Todo__c.Status__c';
+import SUBTODO_OBJECT from '@salesforce/schema/Sub_ToDo__c';
+import SUBTODO_NAME from '@salesforce/schema/Sub_ToDo__c.Name';
+import SUBTODO_PARENTTID from '@salesforce/schema/Sub_ToDo__c.Parent_ToDo__c';
+
+//import getSingleToDo from '@salesforce/apex/ToDoControllerSecond.getSingleToDo';
+//import getSingleSubToDo from '@salesforce/apex/ToDoControllerSecond.getSingleSubToDo';
 
 
 export default class toDo extends LightningElement {
 
-    subToDo;
+    //list todos
+    @api recordId;
+    @api objectApiName;
+    @track uncompletedToDoList;
+    @track completedToDoList;
+    allToDoList;
+    allSubToDoList;
+
+    //
+    toDoName = '';
+    toDoDesccription = '';
+    toDoCloseDate = '';
+    toDoId;
+    priority = 'Low priority';
+
     error;
+
+    subTodoName;
+    subTodoParentID;
+
+    nameToDo = TODO_NAME;
+    descriptionToDo = TODO_DESCRIPTION;
+    closeDateToDo = TODO_CLOSEDATE;
+    priorityToDo = TODO_PRIORITY;
+
+
+
+
     //todos for updating
     singleToDo;
-    singleSubToDo;
-    subToDoList;
+
+    //complete / oncomplete
     status;
 
-    uncompletedToDoList;
-    completedToDoList;
-    allToDoList;
+    //VALUE FOR PICK LISTS IN RECORD CREATE
+    get optionsPriority() {
+        return [
+            { label: 'Low priority', value: 'Low priority' },
+            { label: 'Normal priority', value: 'Normal priority' },
+            { label: 'High priority', value: 'High priority' },
+        ];
+    }
 
-    allSubToDoList;
-    uncompletedSubToDoList;
-    completedToSubDoList;
-
-
-
-
-    handleChange(event) {
-        this.status = event.target.checked;
+    get optionsRecordId() {
+        return [
+            { label: 'Programming', value: 'Normal priority' },
+            { label: 'Testing', value: 'Testing' },
+            { label: 'Design', value: 'High priority' },
+            { label: 'Other', value: 'Other' },
+        ];
     }
 
 
-//call apex methods
+    handleChangePriority(event){
+        this.priority = event.target.value;
+    }
 
+    handleSubmit(event) {
+        console.log('onsubmit event recordEditForm'+ event.detail.fields);
+    }
+    handleAddSubToDoSuccess() {
+
+        this.dispatchEvent(
+            new ShowToastEvent(
+                {
+                    title: 'Success',
+                    message: 'SubToDo is created!',
+                    variant: 'success'
+                })
+        );
+    }
+
+    handleUpdateToDoSuccess(){
+        this.dispatchEvent(
+            new ShowToastEvent(
+                {
+                    title: 'Success',
+                    message: 'ToDo is updated!',
+                    variant: 'success'
+                })
+        );
+        console.log("Update complete");
+    }
 
     handleChangeStatus(event){
         this.status = event.target.checked;
 
     }
 
+    handlerChangeSubTodoName(event){
+        this.subTodoName = event.target.value;
+    }
+    handlerParentToDoId(event){
+        this.subTodoParentID = event.target.value;
+    }
+
+    handleChangeName(event){
+        this.toDoName = event.target.value;
+    }
+
+    handleChangeDescription(event){
+        this.toDoDesccription = event.target.value;
+    }
+
+    handleChangeDateClose(event){
+        this.toDoCloseDate = event.target.value;
+    }
+
+
+
+    saveCreatedToDo() {
+        const fields = {}
+        fields[TODO_NAME.fieldApiName] = this.toDoName,
+            fields[TODO_DESCRIPTION.fieldApiName] = this.toDoDesccription,
+            fields[TODO_PRIORITY.fieldApiName] = this.priority,
+            fields[TODO_CLOSEDATE.fieldApiName] = this.toDoCloseDate;
+        const recordInput = {apiName: TODO_OBJECT.objectApiName, fields};
+        console.log(recordInput);
+        createRecord(recordInput).then((todo) => {
+            this.toDoId = todo.id;
+            this.dispatchEvent(
+                new ShowToastEvent(
+                    {
+                        title: 'Success',
+                        message: 'ToDo created with name: ' + this.toDoName,
+                        variant: 'success'
+                    })
+            );
+        })
+            .catch((error) =>{
+                console.log(JSON.stringify(error));
+            });
+    }
+
+
+
+    //delete for all records
+    deleteToDo(event) {
+        const recordId = event.target.dataset.recordid;
+        deleteRecord(recordId)
+            .then(() => {
+                //refreshLists();
+               getUncompletedToDo();
+               getCompletedToDo();//not working
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error deleting record',
+                        message: reduceErrors(error).join(', '),
+                        variant: 'error'
+                    })
+                );
+            });
+    }
+
+    //create todo
+
 
     //рефрешать через this.getToDoList();
 
     connectedCallback(){
-        getAllToDo()
-            .then(result => {
-                this.allToDoList = result;
-                this.error = undefined;
-            })
-            .catch(error => {
-                this.error = error;
-                this.allToDoList = undefined;
-            });
 
         getUncompletedToDo()
             .then(result => {
@@ -98,86 +214,15 @@ export default class toDo extends LightningElement {
 
 
 
-    deleteToDo(event) {
-        const recordId = event.target.dataset.recordid;
-        deleteRecord(recordId)
-            .then(() => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'Account deleted',
-                        variant: 'success'
-                    })
-                );
-            })
-            .catch((error) => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error deleting record',
-                        message: reduceErrors(error).join(', '),
-                        variant: 'error'
-                    })
-                );
-            });
+    @track openModal = false;
+    showModal() {
+        this.openModal = true;
+    }
+    closeModal() {
+        this.openModal = false;
     }
 
 
-
-
-    /*
-    deleteCompletedToDo(event) {
-        const recordId = event.target.dataset.recordid;
-        event.target.accessKeyLabel
-        deleteRecord(recordId)
-            .then(() => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'Account deleted',
-                        variant: 'success'
-                    })
-                );
-                return refreshApex(this.completedToDoList);
-            })
-            .catch((error) => {
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error deleting record',
-                        message: reduceErrors(error).join(', '),
-                        variant: 'error'
-                    })
-                );
-            });
-    }
-
-
-
-
-    /*   @wire(allToDo)
-  wiredAllToDo(result) {
-      this.allToDoList = result;
-      if (result.data) {
-          this.todoData = result.data;
-          this.error = undefined;
-      } else if (result.error) {
-          this.error = result.error;
-          this.todoData = undefined;
-      }
-  }
-
- @wire(uncompletedToDo)
-  wiredUncompletedToDo(result) {
-  console.log(result, 'result');
-      this.uncompletedToDoList = result;
-      console.log(result.data);
-      if (result.data) {
-          this.todoData = result.data;
-          this.error = undefined;
-      } else if (result.error) {
-          this.error = result.error;
-          this.todoData = undefined;
-      }
-  }*/
 
 
 
